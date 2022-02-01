de5ba89d52983c6c98bf0a5dac736d3bc210bf2d
Anthony Salemo <ajssalemo@gmail.com>
2022-02-01 17:00:11 -0500

    added an example on creating storage accounts

---
+++ java-sdk-resource-creation-samples/create-storage-container/src/main/java/com/createstoragecontainer/azurejavasdk/App.java
@@ -0,0 +1,44 @@
+package com.createstoragecontainer.azurejavasdk;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+
+public class App {
+    private static String storageAccountName = System.getenv("AZURE_STORAGE_ACCOUNT_NAME");
+    private static String rgName = System.getenv("AZURE_RESOURCE_GROUP_NAME");
+
+    public static void createStorageAccount(AzureResourceManager azureResourceManager) {
+        System.out.println("Creating a Storage Account named " + storageAccountName);
+        azureResourceManager.storageAccounts()
+                .define(storageAccountName)
+                .withRegion(Region.US_EAST)
+                .withNewResourceGroup(rgName)
+                .create();
+        System.out.println("Created a Storage Account named " + storageAccountName);
+    }
+
+    public static void main(String[] args) {
+        try {
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                    .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                    .build();
+
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                    .configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(credential, profile)
+                    .withDefaultSubscription();
+
+            createStorageAccount(azureResourceManager);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
