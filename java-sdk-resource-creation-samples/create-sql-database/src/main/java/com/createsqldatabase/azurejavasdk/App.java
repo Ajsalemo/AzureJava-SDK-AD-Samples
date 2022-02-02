a017a8b3e8874bc822489d44bfd0b2f30ad39140
Anthony Salemo <ajssalemo@gmail.com>
2022-02-02 15:34:25 -0500

    adding an example of creating a sql server and then a database for said server

---
+++ java-sdk-resource-creation-samples/create-sql-database/src/main/java/com/createsqldatabase/azurejavasdk/App.java
@@ -0,0 +1,54 @@
+package com.createsqldatabase.azurejavasdk;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.sql.models.SqlServer;
+
+public class App {
+    private static String sqlServerName = System.getenv("AZURE_SQL_SERVER_NAME");
+    private static String sqlDatabaseName = System.getenv("AZURE_SQL_DATABASE_NAME");
+    private static String rgName = System.getenv("AZURE_RESOURCE_GROUP_NAME");
+    private static String administratorLogin = System.getenv("AZURE_SQL_SERVER_ADMIN_LOGIN");
+    private static String administratorPassword = System.getenv("AZURE_SQL_SERVER_ADMIN_PASSWORD");
+
+    public static void createSqlServerAndDatabase(AzureResourceManager azureResourceManager) {
+        System.out.println("Creating a server named " + sqlServerName);
+        SqlServer sqlServer = azureResourceManager.sqlServers().define(sqlServerName)
+                .withRegion(Region.US_EAST)
+                .withNewResourceGroup(rgName)
+                .withAdministratorLogin(administratorLogin)
+                .withAdministratorPassword(administratorPassword)
+                .create();
+        System.out.println("Created a server named " + sqlServerName);
+        System.out.println("Creating a database named " + sqlDatabaseName);
+        sqlServer.databases()
+                .define(sqlDatabaseName)
+                .create();
+        System.out.println("Created a database named " + sqlDatabaseName);
+    }
+
+    public static void main(String[] args) {
+        try {
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                    .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                    .build();
+
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                    .configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(credential, profile)
+                    .withDefaultSubscription();
+
+            createSqlServerAndDatabase(azureResourceManager);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
